a513d802201180e540ecffefe2abe51b929310b6
soumeyahaloui <soumeya.haloui@gmail.com>
2024-12-30 21:09:55 +0100

    updated

--- main.py
+++ main.py
@@ -8,8 +8,6 @@ from kivy.config import Config
 from kivy.core.window import Window 
 
 
-
-
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '720')
