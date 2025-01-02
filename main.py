1138d53b5ec07e98a92e9bc8b75e78ecadde6d0c
soumeyahaloui <soumeya.haloui@gmail.com>
2025-01-02 04:01:50 +0100

    updated

--- main.py
+++ main.py
@@ -2,28 +2,21 @@ import sys
 import os
 from kivy.metrics import dp
 sys.path.append(os.path.join(os.path.dirname(__file__), 'src'))
-
-
 from kivy.config import Config
-from kivy.core.window import Window 
-
+from kivy.core.window import Window
 
 Config.set('graphics', 'resizable', False)
 Config.set('graphics', 'width', '400')
 Config.set('graphics', 'height', '720')
 Config.write()
 
-
 Window.size = (400, 720)
 
-
 from kidsvoc import MyApp
-
 
 def main():
    app = MyApp()
    app.run()
-
 
 if __name__ == '__main__':
    main()
