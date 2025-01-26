43e4844775962db19c75c87be27a8e56fc41b219
soumeyahaloui <soumeya.haloui@gmail.com>
2025-01-26 22:32:23 +0100

    Test commit

--- .buildozer/android/app/src/kidsvoc.py
+++ .buildozer/android/app/src/kidsvoc.py
@@ -2,24 +2,15 @@ import os
 import json
 from kivy.app import App
 from kivy.metrics import dp
-from kivy.config import Config
 from kivy.uix.label import Label
 from kivy.uix.image import Image
-from kivy.uix.popup import Popup
-from kivy.uix.button import Button
 from kivy.core.window import Window
-from kivy.core.text import LabelBase
-from kivy.core.audio import SoundLoader
 from kivy.resources import resource_find
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import ButtonBehavior
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.gridlayout import GridLayout
 from kivy.uix.floatlayout import FloatLayout
 from kivy.graphics import Color, RoundedRectangle
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
-from kivy.uix.carousel import Carousel
-from kivy.clock import Clock
 
 # Load the manifest file
 with open('assets/manifest.json', 'r') as f:
@@ -44,7 +35,7 @@ class CustomButton(ButtonBehavior, FloatLayout):
             color=(1, 1, 1, 1),
             size_hint=(None, None),
             size=self.size,
-            text_size=(self.width - dp(20), None),  # Enable text wrapping
+            text_size=(self.width - dp(20), None),
             pos_hint={'center_x': 0.5, 'center_y': 0.5},
             halign='center',
             valign='middle',
@@ -65,34 +56,31 @@ class FirstScreen(Screen):
     """The main screen displayed on app start."""
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.settings_button = None
         self.init_ui()
 
     def init_ui(self):
+        # Background image
         self.add_widget(Image(
             source=resource_find('assets/images/backgrounds/purple.png'),
             allow_stretch=True, keep_ratio=False
         ))
 
+        # Float layout with settings button
         layout = FloatLayout()
-
-        # Add settings button
-        self.settings_button = IconButton(
+        settings_button = IconButton(
             source='assets/images/icon/settings.png',
             size_hint=(None, None),
             size=(dp(50), dp(50)),
             pos_hint={'right': 0.95, 'top': 0.95}
         )
-        layout.add_widget(self.settings_button)
-
+        layout.add_widget(settings_button)
         self.add_widget(layout)
 
 class MyApp(App):
     """Main application class."""
     def build(self):
-        # Set initial window properties
-        Window.size = (400, 720)
-        Window.clearcolor = (1, 1, 1, 1)
+        # Configure the app to dynamically fit the device's screen
+        Window.clearcolor = (1, 1, 1, 1)  # Set white background
 
         # ScreenManager setup
         sm = ScreenManager(transition=FadeTransition())
